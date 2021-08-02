9a6b56ed7e59fcebc068a4a703755d5d8b94b8b9
Anton Akhramovich <akhramovich.anton@gmail.com>
2021-08-02 23:49:12 +0300

    zabbix: remove function prepare_profile

--- zabbix/entrypoint.sh
+++ zabbix/entrypoint.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-function prepare_profile(){
-	cat <<EOF > ${HOME}/.profile
-WHITE='\[\033[0m\]'
-GREEN='\[\033[1;32m\]'
-PURPURE='\[\033[1;35m\]'
-YELLOW='\[\033[1;33m\]'
-RED='\[\033[0;31m\]'
-BLUE='\[\033[1;34m\]'
-CYAN='\[\033[0;36m\]'
-PS1="\${CYAN}[\u]\${BLUE}@\${CYAN}[\h]\${BLUE}[\W]\${WHITE}\${CYAN}\\$\${WHITE} "
-alias ll='ls -la'
-
-EOF
-}
-
-####################  __MAIN__  ####################
-
 set -e
 
 cd ${HOME}
-
-prepare_profile
 
 # RUN MYSQL
 docker run --name mysql-server -t \
