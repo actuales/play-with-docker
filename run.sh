54d0439230b665055e71f5203ba089c001b18e0f
Anton Akhramovich <akhramovich.anton@gmail.com>
2021-08-03 01:47:36 +0300

    added comment for commands

--- run.sh
+++ run.sh
@@ -5,11 +5,14 @@ ENTRYPOINT_FILE="entrypoint.sh"
 service_name=${1}
 host_name=${2}
 
+### Check args:
 if [[ ! ${service_name} || ! ${host_name} ]];then
 	echo "USAGE: $(basename $0) <SERVICE> <URL_FOR_SSH>"
 	exit 1
 fi
 
+### Copy all service files to remote server:
 scp ${service_name}/* ${host_name}:
 
+### Execute bash script on a remote server:
 ssh ${host_name} "./${ENTRYPOINT_FILE}"
