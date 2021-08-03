6c1aea04313e5b9b48117af7a796c605b6d8af8b
Anton Akhramovich <akhramovich.anton@gmail.com>
2021-08-03 18:17:36 +0300

    added vars for container name and image name

--- jenkins/entrypoint.sh
+++ jenkins/entrypoint.sh
@@ -3,7 +3,8 @@
 PORT_1=8080 	# port for web
 PORT_2=50000 	# port for service
 
-version=${RANDOM}
+CONTAINER_NAME='jenkins'
+IMAGE_NAME='jenkins/jenkins'
 TMP_PASSWORD='/var/jenkins_home/secrets/initialAdminPassword'
 
 function increment_port(){
@@ -27,8 +28,8 @@ function kill_running_container(){
 	[[ ! ${container_name} ]] && exit 1
 	local containers=$(docker ps --format "table {{.Names}}"|grep ${container_name})
 	if [[ ${containers} ]];then
-		echo 'WARNING: Jenkins container already running'
-		if ! is_answer_yes "DIALOG: Do you want to recreate a Jenkins container? [y/N] ";then
+		echo "WARNING: ${CONTAINER_NAME} container already running"
+		if ! is_answer_yes "DIALOG: Do you want to recreate a ${CONTAINER_NAME} container? [y/N] ";then
         	echo "Good bye!" && exit 0
 	    fi
 	fi
@@ -38,28 +39,28 @@ function kill_running_container(){
 	docker system prune -f
 }
 
-kill_running_container jenkins
+kill_running_container ${CONTAINER_NAME}
 
-docker 	run --name jenkins_${version} \
+docker 	run --name ${CONTAINER_NAME} \
 		-p $(increment_port ${PORT_1}):${PORT_1} \
 		-p $(increment_port ${PORT_2}):${PORT_2} \
-		-d jenkins/jenkins
+		-d ${IMAGE_NAME}
 
 sleep 2
 
 docker ps --format "table {{.Names}}\t{{.Networks}}\t{{.Status}}\t{{.Ports}}"
 
 echo
-echo "INFO: Use url below for access to Jenkins:"
+echo "INFO: Use url below for access to ${CONTAINER_NAME}:"
 echo "http://$(wget -q -O- ifconfig.me):${PORT_1}"
 echo
 
 echo "INFO: Waiting for password..."
-while ! docker exec jenkins_${version} ls ${TMP_PASSWORD} &> /dev/null;do
+while ! docker exec ${CONTAINER_NAME} ls ${TMP_PASSWORD} &> /dev/null;do
 	sleep 2
 done
 
 echo
 echo "INFO: Copy/paste password below:"
-docker exec jenkins_${version} cat ${TMP_PASSWORD}
+docker exec ${CONTAINER_NAME} cat ${TMP_PASSWORD}
 echo
