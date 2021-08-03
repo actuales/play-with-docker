47f2dc4715edbd983793e02d97059272cca903be
Anton Akhramovich <akhramovich.anton@gmail.com>
2021-08-03 18:05:28 +0300

    added comment for ports

--- jenkins/entrypoint.sh
+++ jenkins/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PORT_1=8080
-PORT_2=50000
+PORT_1=8080 	# port for web
+PORT_2=50000 	# port for service
 
 version=${RANDOM}
 TMP_PASSWORD='/var/jenkins_home/secrets/initialAdminPassword'
